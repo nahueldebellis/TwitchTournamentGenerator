a3b46808e6a832d2b87d03e178b1b8ff490e3399
nahueldebellis <debellisnahuel@gmail.com>
2021-02-07 04:38:13 -0300

    create src folder and test folder add linter to python, add docstring in methods and class

---
+++ src/tournament.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Tournament class"""
+from random import shuffle
+from url import Url
+
+class Tournament():
+    """this class randomize the participats, and add participants to a file"""
+    def __init__(self):
+        self.filename = '../participantes.txt'
+        self.start = False
+
+    def register_participant(self, participant):
+        """insert in file a new participant"""
+        if self.start:
+            with open(self.filename, "a") as file_participants:
+                file_participants.write(f'{participant}\n')
+        else:
+            raise Exception('no init the tournament')
+
+    def clear_file(self):
+        """clear file of participants"""
+        if self.start:
+            open(self.filename, 'w').close()
+
+    def create_bracket(self):
+        """create brackets and return the url string"""
+        if not self.start:
+            url = Url()
+            with open(self.filename, 'r') as file_participants:
+                persons = file_participants.readlines()
+                shuffle(persons)
+                for person in persons:
+                    url.add(person)
+            return url.show()
+        raise Exception('no init the tournament')
---
+++ src/url.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Url class"""
+class Url():
+    """This class format participants and add to an url"""
+    cant_participants = 0
+    bracket = 0
+    def __init__(self):
+        self.url_final = ['https://scorecounter.com/tournament/?set=31001111000000']
+        self.concat = '&'
+        self.treintaydos = 5
+        self.dieciseis = 4
+        self.ocho = 3
+    def add(self, participant):
+        """add new pasticipant to the bracket"""
+        Url.bracket = Url.bracket if Url.cant_participants % 2 else Url.bracket+1
+        Url.cant_participants = Url.cant_participants+1
+        position = 'home' if Url.cant_participants % 2 else 'visitor'
+        self.url_final.append(f'{self.concat}{position}1-{Url.bracket}={participant}')
+    def show(self):
+        """concat the url and return the string"""
+        Url.cant_participants = 0
+        Url.bracket = 0
+        return ''.join(self.url_final)
