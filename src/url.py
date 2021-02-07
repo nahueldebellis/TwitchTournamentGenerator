9244003d919b1d3c75746306e85bb893cb4aafe8
nahueldebellis <debellisnahuel@gmail.com>
2021-02-07 17:02:25 -0300

    add dependencies in readme, add url shorter to show the bracket in the chat, add dependencie for that pyshorters

--- src/url.py
+++ src/url.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
+from pyshorteners import Shortener
+
 """Url class"""
 class Url():
-    """This class format participants and add to an url"""
+    """This class format participants and add to an url and short the url"""
     cant_participants = 0
     bracket = 0
     def __init__(self):
-        self.url_final = ['https://scorecounter.com/tournament/?set=', '31001111000000']
+        self.short_url = Shortener()
+        self.url_final = ['https://scorecounter.com/tournament/?set=', ' 1001111000000']
         self.concat = '&'
         self.treintaydos = '5'
         self.dieciseis = '4'
+        self.ocho = '3'
     def add(self, participant):
         """add new pasticipant to the bracket"""
         Url.bracket = Url.bracket if Url.cant_participants % 2 else Url.bracket+1
@@ -16,11 +20,13 @@ class Url():
         position = 'home' if Url.cant_participants % 2 else 'visitor'
         self.url_final.append(f'{self.concat}{position}1-{Url.bracket}={participant}')
     def show(self):
-        """concat the url and return the string"""
+        """concat the url and return the string of shorter url"""
         if Url.cant_participants <= 32:
             self.url_final[1] = self.treintaydos+self.url_final[1][1:]
         if Url.cant_participants <= 16:
             self.url_final[1] = self.dieciseis+self.url_final[1][1:]
+        if Url.cant_participants <= 8:
+            self.url_final[1] = self.ocho+self.url_final[1][1:]
         Url.cant_participants = 0
         Url.bracket = 0
-        return ''.join(self.url_final)
+        return self.short_url.isgd.short(''.join(self.url_final))
